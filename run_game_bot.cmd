fd1afae48b0591be316d144848bd363a9c2d4c77
e08s93 <e08s93@gmail.com>
2026-02-21 23:05:04 +0800

    修正log亂碼情形。

--- run_game_bot.cmd
+++ run_game_bot.cmd
@@ -1,17 +1,21 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
+chcp 65001 >nul
 
 cd /d "D:\New_world\auto_bot"
 
 if not exist "logs" mkdir "logs"
-echo [%date% %time%] scheduled run start >> "logs\scheduled_run.log"
+set "LOG_FILE=logs\scheduled_run.log"
+for /f %%I in ('powershell -NoProfile -Command "Get-Date -Format yyyy-MM-dd_HH:mm:ss"') do set "TS=%%I"
+echo [!TS!] scheduled run start >> "!LOG_FILE!"
 
 set "PYTHONUTF8=1"
 set "PYTHONIOENCODING=utf-8"
 
-"C:\WINDOWS\py.exe" -3 -X utf8 "D:\New_world\auto_bot\game_bot.py" >> "logs\scheduled_run.log" 2>&1
+"C:\WINDOWS\py.exe" -3 -X utf8 "D:\New_world\auto_bot\game_bot.py" >> "!LOG_FILE!" 2>&1
 set "EXIT_CODE=%ERRORLEVEL%"
 
-echo [%date% %time%] scheduled run end >> "logs\scheduled_run.log"
+for /f %%I in ('powershell -NoProfile -Command "Get-Date -Format yyyy-MM-dd_HH:mm:ss"') do set "TS=%%I"
+echo [!TS!] scheduled run end (exit=!EXIT_CODE!) >> "!LOG_FILE!"
 
 endlocal & exit /b %EXIT_CODE%
